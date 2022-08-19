d9c48ad0d46f82eebbbda54baa47173d8f4acab3
minhnhat02122001 <lengocminhnhatntp@gmail.com>
2022-08-19 22:17:26 +0700

    Fix car request dto

--- call-center/src/app/shared/interface/car-request.ts
+++ call-center/src/app/shared/interface/car-request.ts
@@ -1,10 +1,20 @@
 export interface carRequest {
     id: string;
     callCenterId: string;
+    customerId: string;
     customerName: string;
     customerPhone: string;
+    driverId: string;
+    driverName: string;
+    driverPhone: string;
     pickingAddress: string;
     carType: string;
-    latPickingAddress?: number;
-    lngPickingAddress?: number;
+    status: string;
+    price: number;
+    distance: number;
+    latPickingAddress: number;
+    lngPickingAddress: number;
+    arrivingAddress: string;
+    lngArrivingAddress: number;
+    latArrivingAddress: number;
 }
